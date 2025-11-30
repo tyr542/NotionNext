2ddbc0b38ccef72172dd5f68cd067c5190ae5873
tyr542 <tyr542@gmail.com>
2025-11-30 17:35:57 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -1,5 +1,3 @@
-import { BeiAnGongAn } from '@/components/BeiAnGongAn'
-import BeiAnSite from '@/components/BeiAnSite'
 import PoweredBy from '@/components/PoweredBy'
 import { siteConfig } from '@/lib/config'
 
@@ -10,20 +8,24 @@ const Footer = ({ title }) => {
   const copyrightDate =
     parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
+  // 移除了 dark:bg-black 和 bg-hexo-light-gray，讓背景變為透明或繼承父層
   return (
-    <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm p-6'>
-      {/* <DarkModeButton/> */}
+    <footer 
+      className='relative z-10 flex-shrink-0 justify-center text-center m-auto w-full leading-6 text-gray-600 dark:text-gray-100 text-sm p-6'
+      // 新增：使用行內樣式 (style) 強制定義繁體中文字體堆棧，確保顯示正確且優美
+      style={{ fontFamily: "'Noto Sans TC', 'PingFang TC', 'Microsoft JhengHei', sans-serif" }}
+    >
       <i className='fas fa-copyright' /> {`${copyrightDate}`}
       <span>
-        <i className='mx-1 animate-pulse fas fa-heart' />
+        {/* 將心形 (fa-heart) 替換為平底鍋 (fa-skillet)。如果你的 Font Awesome 版本不支援 skillet，可能需要換成 fa-utensils (餐具) */}
+        <i className='mx-1 animate-pulse fas fa-skillet' />
         <a
           href={siteConfig('LINK')}
-          className='underline font-bold  dark:text-gray-300 '>
+          className='underline font-bold dark:text-gray-300 '>
           {siteConfig('AUTHOR')}
         </a>
         .<br />
-        <BeiAnSite />
-        <BeiAnGongAn />
+        {/* 備案資訊已移除 */}
         <span className='hidden busuanzi_container_site_pv'>
           <i className='fas fa-eye' />
           <span className='px-1 busuanzi_value_site_pv'> </span>
