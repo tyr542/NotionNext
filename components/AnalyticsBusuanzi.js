895a88a6c433ecf5a4cf7b8f0d99808b5a6aeec5
tyr542 <tyr542@gmail.com>
2026-01-11 20:58:21 +0800

    修復 Busuanzi 統計顯示問題 - 移除 hidden class

--- components/AnalyticsBusuanzi.js
+++ components/AnalyticsBusuanzi.js
@@ -5,11 +5,11 @@
 export default function AnalyticsBusuanzi() {
   return (
     <div className='flex gap-x-1'>
-      <span className='hidden busuanzi_container_site_pv whitespace-nowrap'>
+      <span className='busuanzi_container_site_pv whitespace-nowrap'>
         <i className='fas fa-eye' />
         <span className='px-1 busuanzi_value_site_pv'> </span>
       </span>
-      <span className='hidden busuanzi_container_site_uv whitespace-nowrap'>
+      <span className='busuanzi_container_site_uv whitespace-nowrap'>
         <i className='fas fa-users' />
         <span className='px-1 busuanzi_value_site_uv'> </span>
       </span>
