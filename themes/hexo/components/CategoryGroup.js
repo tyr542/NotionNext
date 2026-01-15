f4b895f5f7bf2cfe4b8297c78df384a9c695fdff
tyr542 <tyr542@gmail.com>
2026-01-15 19:25:23 +0800

    refactor(CategoryGroup): 清除寫死的 Tailwind classes，改用語意化 CSS class

--- themes/hexo/components/CategoryGroup.js
+++ themes/hexo/components/CategoryGroup.js
@@ -17,7 +17,7 @@ const CategoryGroup = ({ currentCategory, categories }) => {
           {locale.COMMON.CATEGORY}
         </div>
 
-        <div id='category-list' className='dark:border-gray-600 flex flex-wrap mx-4'>
+        <div id='category-list' className='category-list'>
           {categories.map(category => {
             const selected = currentCategory === category.name
             return (
@@ -25,13 +25,10 @@ const CategoryGroup = ({ currentCategory, categories }) => {
                 key={category.name}
                 href={`/category/${category.name}`}
                 passHref
-                className={(selected
-                  ? 'hover:text-white dark:hover:text-white bg-indigo-600 text-white '
-                  : 'dark:text-gray-400 text-gray-500 hover:text-white dark:hover:text-white hover:bg-indigo-600') +
-                  ' text-sm w-full items-center duration-300 px-2 cursor-pointer py-1 font-light'}
+                className={`category-item${selected ? ' selected' : ''}`}
               >
                 <div>
-                  <i className={`mr-2 fas ${selected ? 'fa-folder-open' : 'fa-folder'}`} />
+                  <i className={`fas ${selected ? 'fa-folder-open' : 'fa-folder'}`} />
                   {category.name}({category.count})
                 </div>
               </SmartLink>
